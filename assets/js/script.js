37fd556dae57281914618325be7795c3f000ab6c
Ewan Black <ewanblack138@gmail.com>
2023-11-23 22:20:50 +0000

    add loop to checkScore function to check for column and row wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     runGame(box);
                 // If box isn't empty, display message informing user
                 } else {
-                    document.getElementById('info-bar').textContent = 'That box isn\'t empty! Please choose an empty one.';
+                    document.getElementById('info-bar').textContent = 'That box isn\'t empty! Please choose an empty box.';
                 }
             });
         }
@@ -25,10 +25,23 @@ document.addEventListener("DOMContentLoaded", function(){
  * called when event listener is fired after 'click on box
  */
 function runGame(box){
+    // If an empty box is clicked again, it is required to check if the game has already been won
+    if (checkScore() === 'x'){
+        document.getElementById('info-bar').textContent = 'X has already won! Press Restart to play again.';
+        return;
+    }
+
+    if (checkScore() === 'o'){
+        document.getElementById('info-bar').textContent = 'O has already won! Press Restart to play again.';
+        return;
+    }
+
     if (playerMode() === 1){
         takeTurn('x', box);
+        checkScore();
         // Wait 125 ms after player's turn to take computer's turn
         setTimeout(() => computerTurn(), 125);
+        checkScore();
     } else {
         alert('2 player mode functionality yet to be implemented');
     }
@@ -51,9 +64,9 @@ function playerMode(){
  */
 function takeTurn(icon, box){
     if (icon === 'x'){
-        box.innerHTML = '<i class="fa-regular fa-x"></i>'
+        box.innerHTML = '<i class="fa-regular fa-x"></i>';
     } else {
-        box.innerHTML = '<i class="fa-regular fa-o"></i>'
+        box.innerHTML = '<i class="fa-regular fa-o"></i>';
     }
 }
 
@@ -80,4 +93,57 @@ function computerTurn(){
     let computerBox = emptyBoxes[randomNumber];
 
     takeTurn('o', computerBox);
+}
+
+/**
+ * Returns 'x' if X wins and 'o' if O wins
+ * @returns {string}
+ */
+function checkScore(){
+    let rowOne, rowTwo, rowThree, colOne, colTwo, colThree, diagOne, diagThree;
+    let x = '<i class="fa-regular fa-x"></i>';
+    let o = '<i class="fa-regular fa-o"></i>';
+
+    let grid = document.getElementsByClassName('grid-area')[0];
+    // Column counter variable to keep track of column (used to check score in each column)
+    let colCount = 0;
+    let infoBar = document.getElementById('info-bar');
+
+    // Loop to check each row and each column for three consecutive Xs or three consecutive Os
+    for (let row of grid.children){
+        // Gets inner HTML of three boxes in row
+        let firstInRow = row.children[0].innerHTML;
+        let secondInRow = row.children[1].innerHTML;
+        let thirdInRow = row.children[2].innerHTML;
+
+        // Check if X has won in row
+        if (firstInRow === x && secondInRow === x && thirdInRow === x){
+            infoBar.textContent = 'X wins! Press Restart to play again.';
+            return 'x';
+        }
+
+        // Check if O has won in row
+        if (firstInRow === o && secondInRow === o && thirdInRow === o){
+            infoBar.textContent = 'O wins! Press Restart to play again.';
+            return 'o';
+        }
+
+        // Gets inner HTML of three boxes in column to compare to x and o variables in belows 'if' statements
+        let firstInCol = grid.children[0].children[colCount].innerHTML;
+        let secondInCol = grid.children[1].children[colCount].innerHTML;
+        let thirdInCol = grid.children[2].children[colCount].innerHTML;
+        colCount++;
+
+        // Check if X has won in row
+        if (firstInCol === x && secondInCol === x && thirdInCol === x){
+            infoBar.textContent = 'X wins! Press Restart to play again.';
+            return 'x';
+        }
+
+        // Check if O has won in row
+        if (firstInCol === o && secondInCol === o && thirdInCol === o){
+            infoBar.textContent = 'O wins! Press Restart to play again.';
+            return 'o';
+        }
+    }
 }
